9ff729ccedab9e41d9f8321f16cb017ac4ef57e7
mego <megocapps@gmail.com>
2023-05-28 15:53:28 -0400

    db config

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
-/** Database for lunchly */
-
 const pg = require("pg");
 
-const db = new pg.Client("postgresql:///lunchly");
+const db = new pg.Client({
+  user: "megan",
+  password: "mego", 
+  database: "lunchly",
+});
 
 db.connect();
 
